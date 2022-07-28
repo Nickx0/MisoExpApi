3b006c6aca287200bd5e9f098cc273c294730317
Nick Ochoa Sicha <79413378+Nickx0@users.noreply.github.com>
2022-07-28 13:00:41 -0500

    Update subs.controlers.js

--- public/controlers/subs.controlers.js
+++ public/controlers/subs.controlers.js
@@ -1,3 +1,4 @@
+const fetch = require("node-fetch");
 getSubs = async(req,res)=>{
     data = await fetch(`https://www.youtube.com/channel/UC6tSB9TnO0f01OBeo9UEJZA/`);
     data = await data.text();

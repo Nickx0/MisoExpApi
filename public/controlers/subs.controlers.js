ecde8232e87a7c1384455969ef832df5e078c78f
Nick Ochoa Sicha <79413378+Nickx0@users.noreply.github.com>
2022-07-28 12:59:21 -0500

    add subs controler

---
+++ public/controlers/subs.controlers.js
@@ -0,0 +1,7 @@
+getSubs = async(req,res)=>{
+    data = await fetch(`https://www.youtube.com/channel/UC6tSB9TnO0f01OBeo9UEJZA/`);
+    data = await data.text();
+    data = data.match(/{"label":(.+?)}/g).filter(d=>d.includes("suscriptores"));
+    res.json(data.pop())
+};
+module.exports = getSubs;

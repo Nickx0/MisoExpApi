09b0a2af6988ff6a2d5a4a1ff379cc170430b3d7
Nick Ochoa Sicha <79413378+Nickx0@users.noreply.github.com>
2022-07-28 13:19:13 -0500

    Update subs.controlers.js

--- public/controlers/subs.controlers.js
+++ public/controlers/subs.controlers.js
@@ -1,7 +1,9 @@
 const Axios = require("axios").default;
 getSubs = async(req,res)=>{
     data = await Axios(`https://www.youtube.com/channel/UC6tSB9TnO0f01OBeo9UEJZA/`);
+    console.log(data)
     data = data.data.match(/{"label":(.+?)}/g).filter(d=>d.includes("suscriptores"));
+    console.log(data)
     res.json(data.pop())
 };
 module.exports = getSubs;
